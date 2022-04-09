2e3d5a3dddbc770b8528facc4f3b8b6bb65bdbee
user <user@DESKTOP-SGQ3N6M>
2022-04-09 11:21:05 +0900

    출력문 추가 - 김지희

--- src/app/MainTest.java
+++ src/app/MainTest.java
@@ -7,6 +7,9 @@ public class MainTest {
 		System.out.println("test");
 		//git-hub에서 추가한 내용
 		System.out.println("git-hub");
+		
+		//spring에서 추가한 내용 - 김지희
+		System.out.println("test 김지희");
 	}
 
 }
