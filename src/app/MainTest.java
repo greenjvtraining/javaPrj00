ff9018307be2650843b2f75199fb4ae4cd2eee3f
greenjvtraining <102376565+greenjvtraining@users.noreply.github.com>
2022-04-09 10:45:56 +0900

    Update MainTest.java
    출력문 추가

--- src/app/MainTest.java
+++ src/app/MainTest.java
@@ -5,6 +5,8 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
+		//git-hub에서 추가한 내용
+		System.out.println("git-hub");
 	}
 
 }
